cae9ac95a0fae7d111997b205fb3752700da6b05
system205 <arsenmutalapov@gmail.com>
2022-06-10 10:25:02 +0300

    send request with codeword to server which receives

--- student.js
+++ student.js
@@ -2,7 +2,21 @@ const form = document.getElementById('code-word-form');
 
 form.onsubmit = () => {
     const codeWord = form.getElementsByTagName('input')[0].value;
+    let valid = false;
 
+    //check the presence of the codeWord
+    fetch('/api/codeword', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            codeword : codeWord
+        })
+    })
+    .then(data => valid = data.text());
 
-    return false;
+    console.log(valid);
+
+    return valid;
 }
