c496bc2e68e6bd23049f3276c2b6166862e86edd
system205 <arsenmutalapov@gmail.com>
2022-06-10 15:16:31 +0300

    add creation of blocks for session on site

--- dashboard.js
+++ dashboard.js
@@ -1,15 +1,31 @@
 console.log('hello world!')
 
 const newSessionButton = document.getElementById('new-session-button');
-newSessionButton.addEventListener('click', () => {
-    console.log('Hello');
+newSessionButton.addEventListener('click', async () => {
     const title = prompt('Enter the title of a new session', '');
-    const id = fetch('/api/feedback', {
+    console.log(title);
+    const response = await fetch('/api/feedback', {
         method: 'POST',
         headers: {
             'Content-Type' : 'application/json'
         },
         body: JSON.stringify({title: title})
     }).then(data => data.json());
-    console.log(id);
+    console.log(response); // json body contains ID for creating new session on the page
+
+    if(title != null && title.trim() !== '')
+    {
+        const sessionList = document.getElementById('session-list');
+        console.log(sessionList.childNodes);
+
+        const newSession = document.createElement('div');
+        newSession.id = response.id;
+        newSession.classList.add('session');
+        newSession.innerHTML = `<div class="session-header"><span class="session-title">${title}</span><span class="session-time">17:49</span></div><div class="session-content"><span>Feedback text here</span></div>`;
+        console.log(newSession);
+        const button = document.getElementById('new-session-button');
+        console.log(button);
+        sessionList.insertBefore(newSession, button);
+    }
+
 })
