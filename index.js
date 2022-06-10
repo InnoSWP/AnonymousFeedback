b5ab01167832f6673da54d6cd5630d532630857c
system205 <arsenmutalapov@gmail.com>
2022-06-10 10:25:02 +0300

    send request with codeword to server which receives

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 const path = require('path');
 
 const PORT = process.env.PORT || 5000;
@@ -10,6 +11,19 @@ app.use(express.json()) // for a server to accept json format
 app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, 'index.html'));
 })
+app.get('/student.js', (request, response) => {
+    response.sendFile(path.join(__dirname, 'student.js'));
+})
+app.post('/api/codeword', (request, response) => {
+    
+    const codeWord = request.body.codeword;s
+    console.log(codeWord);
+
+    //NEED LOGIC TO SEND flag whether codeword is in DB or not
+    
+    response.end();
+})
+
 
 const start = () => {
     try {
