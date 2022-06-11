2cf2e880313a8f8623fb90e7c55cf87dcd7e984f
system205 <arsenmutalapov@gmail.com>
2022-06-11 16:25:05 +0300

    add logic (random login and password) to authorize like a guest

---
+++ guest.js
@@ -0,0 +1,34 @@
+onload = () => {
+    const guestButton = document.getElementById('guest-button');
+    guestButton.addEventListener('click', () => {
+        const login = generateRandomString(32);
+        const password = generateRandomString(32);
+
+        
+
+        const [name, surname] = prompt('Enter your name please', 'Jhon Smith').split(' ');
+        fetch('/dashboard', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({name: name, surname: surname, login: login, password:password})
+        })
+
+    })
+
+    const backButton = document.getElementById('back-button');
+    backButton.addEventListener('click', () => {
+        fetch('/').then(response => window.location.href = response.url);
+    })
+}
+
+function generateRandomString(length) {
+    let result           = '';
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+   return result;
+}
