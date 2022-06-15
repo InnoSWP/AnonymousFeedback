f13c62e943158361c0a9e62832cf51d688251dda
system205 <arsenmutalapov@gmail.com>
2022-06-15 21:33:10 +0300

    fix code typos

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const io = require('socket.io', 4000, {
-  cors: [
-    origin: '*', 
+const io = require('socket.io')(4000, {
+  cors: {
+    origin: '*',
     methods: ["GET", "POST"]
-  ]
+  }
 });
 
 io.on('connection', socket => {
   // the client emits the send-message event 
   // codeword represents the ID of the TA 
-  socket.on('send-message', (codeword, message) {
-    var date = new Date();
-    var time = date.getHours() + ":" + date.getMinutes();
+  console.log(socket.id);
+  socket.on('send-message', (codeword, message) => {
+    let date = new Date();
+    let time = date.getHours() + ":" + date.getMinutes();
     socket.to(codeword).emit("receive-message", message, time); // emits 
     // message & time to the room of the TA, using the codeword as the ID 
-  })  
+  })
 })
