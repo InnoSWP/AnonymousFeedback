038bb888922615aad09d934c8e7fb3af6d9f013b
Rail <r.sayfeev@yandex.ru>
2022-06-15 19:53:42 +0300

    Added function to add messages to the dashboard + scrollbar. Modified: dashboard.js  , dashboard.html , dashboard.css

--- dashboard.js
+++ dashboard.js
@@ -1,31 +1,74 @@
 console.log('hello world!')
 
-const newSessionButton = document.getElementById('new-session-button');
-newSessionButton.addEventListener('click', async () => {
-    const title = prompt('Enter the title of a new session', '');
-    console.log(title);
-    const response = await fetch('/api/feedback', {
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: JSON.stringify({title: title})
-    }).then(data => data.json());
-    console.log(response); // json body contains ID for creating new session on the page
+function addMessage(feedback){
+    const feedbackList = document.getElementById('feedback-list');
+    const newMessage = document.createElement('div');
+    newMessage.classList.add('feedback-item');
 
-    if(title != null && title.trim() !== '')
-    {
-        const sessionList = document.getElementById('session-list');
-        console.log(sessionList.childNodes);
+    newMessage.innerHTML = `<div class="feedback-item-header">
+                                <div class="feedback-name">AnonGuest</div>
+                                <div class="feedback-time">${feedback.time}</div>
+                            </div>
+                            <div class="feedback-content">
+                                <div>${feedback.text}</div>
+                           </div>`;
+    console.log(newMessage);
 
-        const newSession = document.createElement('div');
-        newSession.id = response.id;
-        newSession.classList.add('session');
-        newSession.innerHTML = `<div class="session-header"><span class="session-title">${title}</span><span class="session-time">17:49</span></div><div class="session-content"><span>Feedback text here</span></div>`;
-        console.log(newSession);
-        const button = document.getElementById('new-session-button');
-        console.log(button);
-        sessionList.insertBefore(newSession, button);
-    }
+    feedbackList.insertAdjacentElement("afterbegin", newMessage);
+}
 
-})
+const test = {time: "11:11", text: "Nice"};
+
+addMessage(test);
+addMessage(test);
+addMessage(test);
+addMessage(test);
+addMessage(test);
+addMessage(test);
+addMessage(test);
+addMessage(test);
+addMessage(test);
+
+
+
+
+
+
+
+
+// const newSessionButton = document.getElementById('new-session-button');
+// newSessionButton.addEventListener('click', async () => {
+//     const title = prompt('Enter the title of a new session', '');
+//     console.log(title);
+//     const response = await fetch('/api/feedback', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type' : 'application/json'
+//         },
+//         body: JSON.stringify({title: title})
+//     }).then(data => data.json());
+//     console.log(response); // json body contains ID for creating new session on the page
+//
+//     if(title != null && title.trim() !== '')
+//     {
+//         const sessionList = document.getElementById('session-list');
+//         console.log(sessionList.childNodes);
+//
+//         const newSession = document.createElement('div');
+//         newSession.id = response.id;
+//         newSession.classList.add('session');
+//         newSession.innerHTML = `<div class="session-header">
+//                                     <span class="session-title">${title}</span>
+//                                     <span class="session-time">17:49</span>
+//                                 </div>
+//                                 <div class="session-content">
+//                                     <span>Feedback text here</span>
+//                                 </div>`;
+//         console.log(newSession);
+//         const button = document.getElementById('new-session-button');
+//         console.log(button);
+//         sessionList.insertBefore(newSession, button);
+//     }
+//
+//
+// })
