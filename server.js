91c1155f866bf0f1740ce7090e033a4d283c35f0
system205 <arsenmutalapov@gmail.com>
2022-06-15 21:34:55 +0300

    change sent parameters

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ const io = require('socket.io')(4000, {
 io.on('connection', socket => {
   // the client emits the send-message event 
   // codeword represents the ID of the TA 
-  console.log(socket.id);
   socket.on('send-message', (codeword, message) => {
     let date = new Date();
     let time = date.getHours() + ":" + date.getMinutes();
-    socket.to(codeword).emit("receive-message", message, time); // emits 
+    socket.to(codeword).emit("receive-message", {text: message, time: time}); // emits 
     // message & time to the room of the TA, using the codeword as the ID 
   })
 })
