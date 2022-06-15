8c406e62b36b76b39e2d66ff08929030ee7d0131
system205 <arsenmutalapov@gmail.com>
2022-06-15 19:15:53 +0300

    delete previous logic for guest and back buttons

--- guest.js
+++ guest.js
@@ -1,34 +1,11 @@
-onload = () => {
-    const guestButton = document.getElementById('guest-button');
-    guestButton.addEventListener('click', () => {
-        const login = generateRandomString(32);
-        const password = generateRandomString(32);
 
-        
+const guestButton = document.getElementById('guest-button');
+guestButton.addEventListener('click', () => {
+   location.href = '/dashboard';
+})
 
-        const [name, surname] = prompt('Enter your name please', 'Jhon Smith').split(' ');
-        fetch('/dashboard', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({name: name, surname: surname, login: login, password:password})
-        })
+const backButton = document.getElementById('back-button');
+backButton.addEventListener('click', () => {
+    location.href = '/';
+})
 
-    })
-
-    const backButton = document.getElementById('back-button');
-    backButton.addEventListener('click', () => {
-        fetch('/').then(response => window.location.href = response.url);
-    })
-}
-
-function generateRandomString(length) {
-    let result           = '';
-    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-   return result;
-}
