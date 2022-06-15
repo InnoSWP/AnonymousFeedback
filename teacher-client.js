e2c38b5a11611a9db34837ef10b6d467f44c7c33
system205 <arsenmutalapov@gmail.com>
2022-06-15 19:12:32 +0300

    connect teacher to the server

---
+++ teacher-client.js
@@ -0,0 +1,17 @@
+const URL = 'http://localhost:4000'; // server socket.io
+
+const io = require('socket.io-client');
+const socket = io(URL);
+
+const linkText = document.getElementById('link-field');
+const link = 'http://localhost:5000/feedback?codeword='; // for student to enter the feedback
+linkText.value = 'Server is disconnected, sorry';
+
+socket.on('connect', () => {
+    console.log('connected to server')
+    const codeword = socket.id;
+
+    linkText.value = link+codeword;
+
+    console.log(linkText.value)
+})
