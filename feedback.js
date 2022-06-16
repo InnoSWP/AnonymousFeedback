e74a6d18f7771f32a776863a2cb51f9a7cbc2b36
system205 <arsenmutalapov@gmail.com>
2022-06-16 16:43:37 +0300

    send message to the server

---
+++ feedback.js
@@ -0,0 +1,30 @@
+const URL = 'http://localhost:4000'; // server socket.io
+
+const io = require('socket.io-client');
+const socket = io(URL);
+
+const feedbackTextField = document.getElementById('feedbackText');
+const form = document.getElementById('form');
+
+const query = new Proxy(new URLSearchParams(window.location.search), {
+  get: (params, prop) => params.get(prop),
+});
+let codeword = query.codeword;
+
+socket.on('connect', () => {
+  console.log('You are successfully connected');
+})
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (feedbackTextField.value.trim() != "") {
+    console.log(`You sent: "${feedbackTextField.value}" to session with codeword: "${codeword}"`);
+    socket.emit('send-message', codeword, feedbackTextField.value);
+    feedbackTextField.value = "";
+  }
+  
+})
+
+
+
