7c664b0c2b63819885961717ed8cfbeae39a38ba
system205 <arsenmutalapov@gmail.com>
2022-06-22 23:17:52 +0300

    add saving name in database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./index');
 const http = require('http').createServer(app);
 const getCodeword = require('./codewordSet');
+const { updateSession, addSession } = require('./database/mongodb');
 module.exports = {
   start: () => {
     const io = require('socket.io')(http, {
@@ -9,13 +10,14 @@ module.exports = {
       }
     });
 
-    io.use((socket, next) => {
+    io.use(async (socket, next) => {
       console.log('Parameters from client:', socket.handshake.auth);
       if (socket.handshake.auth.id) {
         console.log(`Change from ${socket.id} to ${socket.handshake.auth.id}`)
         socket.id = socket.handshake.auth.id
       } else {
         socket.id = getCodeword();
+        await addSession({ codeword: socket.id });
       }
       next();
     })
@@ -37,7 +39,10 @@ module.exports = {
         socket.to(codeword).emit("receive-message", { text: message, time: time }); // emits 
         // message & time to the room of the TA, using the codeword as the ID 
       })
-      socket.on('update-session', (codeword, data) => { console.log(`Codeword: ${codeword} with data: ${JSON.stringify(data)}`) });
+      socket.on('update-session', (codeword, data) => {
+        console.log(`Codeword: ${codeword} with data: ${JSON.stringify(data)}`);
+        updateSession(codeword, data);
+      });
     })
   }, http
 }
