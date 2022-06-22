87aac8df84aec8544655d6554648ede5472491a7
system205 <arsenmutalapov@gmail.com>
2022-06-22 23:17:52 +0300

    add saving name in database

--- submitTitleName.js
+++ submitTitleName.js
@@ -5,10 +5,12 @@ import { socket } from './teacher-client';
 export const nameEvent = () => {
     document.getElementById('name-form').addEventListener('submit', (e) => {
         e.preventDefault();
-        const name = document.getElementById('name').value;
+        const nameField = document.getElementById('name');
+        const name = nameField.value;
         console.log(`New name: ${name}`);
 
         const codeword = getCookie('token');
         socket.emit('update-session', codeword, { teacher: name });
+        nameField.blur();
     })
 }
