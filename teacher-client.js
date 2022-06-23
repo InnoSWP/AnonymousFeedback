b08329d0ae21d9a3b86a21c29eb89c3c8fa25c4e
system205 <arsenmutalapov@gmail.com>
2022-06-23 17:15:47 +0300

    refactor and add init of teacher after page reloading:

--- teacher-client.js
+++ teacher-client.js
@@ -1,6 +1,6 @@
 const URL = 'http://localhost:5000'; // server socket.io
 
-import { addMessage, copyEvent } from './dashboard';
+import { addMessage, copyEvent, updateLink } from './dashboard';
 import { nameEvent } from './submitTitleName';
 nameEvent();
 copyEvent();
@@ -9,14 +9,9 @@ export const socket = io(URL, {
     autoConnect: false,
 });
 
-const linkText = document.getElementById('link-field');
-const link = 'http://localhost:5000/feedback?codeword='; // for student to enter the feedback
-linkText.value = 'Server is disconnected, sorry';
-
+//Restore token
 let token = getCookie('token'); // extract value of token from cookie
-if (token) {
-    socket.auth = { id: token }; // to update id on the server
-}
+if (token) socket.auth = { id: token }; // to update id on the server
 socket.connect();
 
 socket.on('connect', () => {
@@ -24,13 +19,16 @@ socket.on('connect', () => {
     if (!document.cookie.token) {
         document.cookie = 'token=' + socket.id; // save for future restoring
     }
-    const codeword = socket.id;
-    linkText.value = link + codeword;
 
     socket.on('receive-message', (feedback) => {
         addMessage(feedback);
     })
 
+    socket.on('init', (codeword, messages) => {
+        console.log('Init from server:', codeword, messages)
+        updateLink(codeword);
+        if (messages) messages.forEach(feedback => addMessage(feedback));
+    })
 })
 socket.onAny((event, ...args) => {
     console.log(event, args);
